d2616fb2792401576189db86747950bb81cd162f
hadjehaouamahamatissa <hayjmhti@gmail.com>
2025-11-06 14:36:26 +0100

    Implementation des routes pour le differentes pages du systeme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { LanguageProvider } from "./contexts/LangContext";
 import LandingPage from "./pages/LandingPage"
 import Home from "./pages/Dashboard"
 import Layout from "./components/Layout"
@@ -8,22 +9,26 @@ import Marketplace from "./pages/Marketplace"
 import Finance from "./pages/Finance"
 import Resources from "./pages/Resources"
 import LoginPage from "./pages/LoginPage"
+import VerifyOTP from "./pages/VerifyOTP"
 // import SignupPage from "./pages/SignupPage"
 
 function App() {
     return (
+        <LanguageProvider>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 {/* <Route path="/signup" element={<SignupPage />} /> */}
                 <Route path="/home" element={<Layout><Home /></Layout>} />
+                <Route path="/verify-otp" element={<VerifyOTP />} />
                 <Route path="/cooperatives" element={<Layout><Cooperatives /></Layout>} />
                 <Route path="/marketplace" element={<Layout><Marketplace /></Layout>} />
                 <Route path="/finance" element={<Layout><Finance /></Layout>} />
                 <Route path="/resources" element={<Layout><Resources /></Layout>} />
             </Routes>
         </BrowserRouter>
+        </LanguageProvider>
     )
 }
 
