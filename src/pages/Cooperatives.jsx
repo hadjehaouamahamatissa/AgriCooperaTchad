c21f97308a2a69fbe3dbe13809820fc3a768d17f
hadjehaouamahamatissa <hayjmhti@gmail.com>
2025-11-12 14:06:41 +0100

    Implementation du systeme de gestion des cooperatives

--- src/pages/Cooperatives.jsx
+++ src/pages/Cooperatives.jsx
@@ -84,12 +84,12 @@ const CooperativeModal = ({ isOpen, onClose, cooperative, onSave }) => {
             email: "",
             dateCreation: new Date().toISOString().split('T')[0],
             isActive: true,
-            membres: [],
-            statistiques: {
-                nombresMembres: 0,  // Vous pourrez modifier cette valeur
-                chiffreAffaireAnnuel: 0,
-                superficieTotal: 0
-            }
+            // membres: [],
+            // statistiques: {
+            //     nombresMembres: 0,  // Vous pourrez modifier cette valeur
+            //     chiffreAffaireAnnuel: 0,
+            //     superficieTotal: 0
+            // }
         }
     )
 
@@ -166,7 +166,7 @@ const CooperativeModal = ({ isOpen, onClose, cooperative, onSave }) => {
                         />
                     </div>
 
-                    <div>
+                    {/* <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">
                             Nombre de membres initial
                         </label>
@@ -183,7 +183,7 @@ const CooperativeModal = ({ isOpen, onClose, cooperative, onSave }) => {
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             min="0"
                         />
-                    </div>
+                    </div> */}
 
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">
